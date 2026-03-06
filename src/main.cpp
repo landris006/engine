abc15039a84ed143c18eb40097586197bf44ae43
landris006 <laczko.andris007@gmail.com>
2026-03-06 23:04:59 +0000

    misc

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,10 @@
-#include "vulkan/vulkan.hpp"
-#include <vulkan/vulkan.hpp>
-
 #include <GLFW/glfw3.h>
 #include <imgui.h>
 #include <imgui_impl_glfw.h>
 #include <imgui_impl_vulkan.h>
 #include <utils.h>
+
+#include <vulkan/vulkan.hpp>
 
 int main() {
   glfwSetErrorCallback(glfw_error_callback);
@@ -14,16 +13,14 @@ int main() {
   }
 
   glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-  GLFWwindow *window = glfwCreateWindow(1280, 800, "Vulkan.hpp + Slang + ImGui",
+  GLFWwindow* window = glfwCreateWindow(1280, 800, "Vulkan.hpp + Slang + ImGui",
                                         nullptr, nullptr);
 
-  // 2. Use vulkan.hpp structures (Exceptions are thrown on failure by default)
   vk::ApplicationInfo appInfo("Vulkan.hpp Engine", 1, "No Engine", 1,
                               VK_API_VERSION_1_3);
+
   vk::InstanceCreateInfo createInfo({}, &appInfo);
 
-  // Using RAII handles automatically cleans up the instance when it goes out of
-  // scope
   vk::UniqueInstance instance = vk::createInstanceUnique(createInfo);
 
   vk::UniqueDevice device =
--- src/utils.h
+++ src/utils.h
@@ -1,6 +1,19 @@
 #pragma once
-#include <cstdio>
+#include <vulkan/vulkan_core.h>
 
-static void glfw_error_callback(int error, const char *description) {
+#include <cstdio>
+#include <cstdlib>
+
+static void glfw_error_callback(int error, const char* description) {
   fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+}
+
+static void vk_check(VkResult err) {
+  if (err == VK_SUCCESS) {
+    return;
+  }
+  fprintf(stderr, "[vulkan] Error: VkResult = %d\n", err);
+  if (err < 0) {
+    std::abort();
+  }
 }
